2262424f5b00f14f685696b96b8767a2670e0230
renata-souza <renatansmto@gmail.com>
2023-01-29 21:29:51 -0300

    feat: add comments functionality

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../../UserContext'
+import PhotoCommentsForm from './PhotoCommentsForm'
+import styles from './PhotoComments.module.css'
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments)
+  const {login} = useContext(UserContext)
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <ul className={styles.comments}>
+        {comments.map(comment => (
+          <li key={comment.comment_ID}>
+            <strong>{comment.comment_author}: </strong>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
   )
 }
 
--- src/api.js
+++ src/api.js
@@ -83,3 +83,19 @@ export function PHOTO_GET(id) {
   };
 }
 
+export function COMMENT_POST(id, body, token) {
+  return {
+    url: `${API_URL}/api/comment/${id}`,
+    options: {
+      method: 'POST',
+      cache: 'no-store',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+      body: JSON.stringify(body)
+    },
+  };
+}
+
+
