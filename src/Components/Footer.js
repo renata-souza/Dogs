0ed84977db08818fcb27347eef1ab9de23f308a0
renata-souza <renatansmto@gmail.com>
2023-02-20 00:13:11 -0300

    feat: add footer styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from '../Styles/Footer.module.css'
+import {ReactComponent as Dogs} from '../Assets/dogs-footer.svg'
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      
-    </div>
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns Direitos Reservados</p>
+    </footer>
   )
 }
 
